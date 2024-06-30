17e35a002268b35b9a55f9deb6a3160b91e5d981
Jai Pal <jaipal.saini.0895@gmail.com>
2024-06-30 22:29:28 +0530

    Basic car model

---
+++ BasicCar/Nano.java
@@ -0,0 +1,23 @@
+package PrototypePattern.BasicCar;
+
+import java.util.Random;
+
+class Nano extends BasicCar {
+    public Nano(String modelName) {
+        this.modelName = modelName;
+        // The base price for a Nano car
+        basePrice = 5000;
+        // Set the on-road price
+        onRoadPrice = basePrice + (new Random()).nextInt(1000);
+    }
+
+    @Override
+    public BasicCar clone() throws CloneNotSupportedException {
+        return (Nano) super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "Model: " + modelName + "\nPrice: " + onRoadPrice;
+    }
+}
